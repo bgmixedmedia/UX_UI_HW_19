27e871048cd40cfbb5a27532ff9193583fd5d134
bgmixedmedia <68300137+bgmixedmedia@users.noreply.github.com>
2020-12-20 17:49:34 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 console.log("Hello world!");
+
+$(document).ready(function(){
+    $(".button1").hover(function(){
+      $(this).css("background-color", "#D66D69");
+      $(this).css("color", "#000000");
+    });
+    $(".button1").click(function(){
+        $(this).css("background-color", "#D94E49");
+        $(this).css("color", "#000000");
+      });
+
+      $(".button2").hover(function(){
+        $(this).css("background-color", "#D66D69");
+        $(this).css("color", "#000000");
+      });
+      $(".button2").click(function(){
+          $(this).css("background-color", "#D94E49");
+          $(this).css("color", "#000000");
+        });
+  });
